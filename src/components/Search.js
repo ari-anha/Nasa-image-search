238c568c00c83d7df3324cd09b4cd24eedd9cb8a
Arianha Bayley <arianhamb@gmail.com>
2020-08-15 14:54:11 +0100

    Added a submit buttom to the search component. Also updated the test snapshots.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
+import { GrFormSearch } from "react-icons/gr";
 import "../styles/Search.css";
 
 const Search = () => {
+  const [value, setValue] = useState("");
   return (
     <>
-      <input className="search" type="text" />
+      <form>
+        <input
+          className="searchInput"
+          type="text"
+          onChange={(event) => setValue(event.target.value)}
+        />
+        <button className="submitButton" typeof="submit">
+          <GrFormSearch />
+        </button>
+      </form>
     </>
   );
 };
